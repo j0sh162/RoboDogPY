312241d86fbeb545f964b42e3eab090c4663591c
j0sh162 <joshkramer567@gmail.com>
2024-01-10 16:32:54 +0100

    Working on the task space control sim

--- Manipulator.py
+++ Manipulator.py
@@ -1,52 +1,85 @@
-# import pybullet as p
-# import time
-# import pybullet_data
-# physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
-# p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
-# p.setGravity(0,0,-10)
-# planeId = p.loadURDF("plane.urdf")
-# startPos = [0,0,1]
-# startOrientation = p.getQuaternionFromEuler([0,0,0])
-# boxId = p.loadURDF("r2d2.urdf",startPos, startOrientation)
-# #set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
-# for i in range (10000):
-#     p.stepSimulation()
-#     time.sleep(1./240.)
-# cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
-# print(cubePos,cubeOrn)
-# p.disconnect()
-
-
 import pybullet as p
-
-# Initialize PyBullet and load your robot
-p.connect(p.GUI)
-robot_id = p.loadURDF("path/to/your/robot.urdf", [0, 0, 0])
-
-# Get the number of joints in the robot
-num_joints = p.getNumJoints(robot_id)
+import pybullet_data
+import numpy as np
+import kinpy as kp
 
 
-# Set the joint positions to some arbitrary values
-joint_positions = [0.0] * num_joints
-for joint_index in range(num_joints):
-    p.resetJointState(robot_id, joint_index, joint_positions[joint_index])
+def get_joint_positions(p,robot_id,num_joints):
+    return [p.getJointState(kukaId, jointIndex)[0] for jointIndex in range(numJoints)]
 
-# Get the robot's current state
-state = p.getJointStates(robot_id, range(num_joints))
+def get_joint_velocities(p,robot_id,num_joints):
+    return [p.getJointState(kukaId, jointIndex)[1] for jointIndex in range(numJoints)]
 
-# Calculate the Jacobian of the center of mass
-link_indices = range(num_joints)  # You may need to adjust these indices based on your robot
-com_jacobian, com_velocity, com_reaction_forces, com_torques = p.calculateJacobian(
-    bodyUniqueId=robot_id,
-    linkIndex=-1,  # -1 refers to the center of mass
-    localPosition=[0, 0, 0],  # Center of mass position in the link's frame
-    objPositions=state[0],  # Joint positions
-    objVelocities=state[1],  # Joint velocities
-    objAccelerations=state[2],  # Joint accelerations
-    physicsClientId=0
-)
 
-# Print or use the center of mass Jacobian matrix as needed
-print("Center of Mass Jacobian:")
-print(com_jacobian)
+def calc_com_jac(p,robot_id, end_effector_id,numJoints):
+    jacobians = []
+    for i in range(end_effector_id):
+        result = p.getLinkState(kukaId, i, computeLinkVelocity=1, computeForwardKinematics=1)
+        link_trn, link_rot, com_trn, com_rot, frame_pos, frame_rot, link_vt, link_vr = result
+        jacobians.append(p.calculateJacobian(robot_id,i,com_trn,get_joint_positions(p,robot_id,numJoints)[1:5],get_joint_velocities(p,robot_id,numJoints)[1:5],[0,0,0,0]))
+
+
+    mass = []
+
+    for i in range(numJoints):
+        mass.append(p.getDynamicsInfo(kukaId,i)[0])
+
+    total_mass = sum(mass)
+
+    com_jac = np.zeros(jacobians[0].shape)
+
+    for i in range(len(jacobians)):
+        com_jac += jacobians[i] * mass[i]/total_mass
+
+    return com_jac
+
+def calc_com(p,robot_id):
+    return None
+
+
+
+
+clid = p.connect(p.SHARED_MEMORY)
+if (clid < 0):
+    p.connect(p.DIRECT)
+
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+time_step = 0.001
+gravity_constant = -9.81
+p.resetSimulation()
+p.setTimeStep(time_step)
+p.setGravity(0.0, 0.0, gravity_constant)
+
+p.loadURDF("plane.urdf", [0, 0, -0.3])
+
+# kukaId = p.loadURDF("TwoJointRobot_w_fixedJoints.urdf", useFixedBase=True)
+kukaId = p.loadURDF("arm.urdf", useFixedBase=True)
+p.resetBasePositionAndOrientation(kukaId, [0, 0, 0], [0, 0, 0, 1])
+numJoints = p.getNumJoints(kukaId)
+
+model = kp.build_serial_chain_from_urdf(open("arm.urdf"),'arm_link_5')
+jacobians = model.jacobian([0,0,0,0],end_only=False)
+i = 0
+mass = []
+for i in range(7):
+    mass.append( p.getDynamicsInfo(kukaId,i-1)[0])
+total_mass = sum(mass)
+keys = jacobians.keys()
+
+
+com_jac = np.zeros(jacobians['base_link'].shape)
+counter = 0
+for i in keys:
+    com_jac += jacobians[i] * mass[counter]/total_mass
+    counter += 1
+
+
+com_jac_inv = np.linalg.pinv(com_jac)
+
+
+V_task = np.array([2,1,1,0,0,0])
+
+joint_velocities = np.dot(com_jac_inv, V_task)
+print(joint_velocities)
+
